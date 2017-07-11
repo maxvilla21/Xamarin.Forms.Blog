209f7392ad90c0e655cb157d55634c5c9c4b635d
Maximiliano Villa <max_210291@hotmail.com>
2017-07-10 23:46:44 -0500

    Code

---
+++ AppConvertersXF/AppConvertersXF/AppConvertersXF/ValueConverters/TextToBoolConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AppConvertersXF.ValueConverters
+{
+    class TextToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = false;
+            //verificar el valor de "EntStatus" en "Text" contenga algun valor.
+            //cuando se le paso como parametro.
+            if (value != null)
+            {
+                //pasando a minusculas el texto 
+                //y enseguida verificar si coincide con los casos
+                switch (value.ToString().ToLower())
+                {
+                    case "activo":
+                        result = true;
+                        break;
+                    case "inactivo":
+                        result = false;
+                        break;
+                }
+            }
+            return result;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string result = "";
+            //vericar que el objeto valor sea boleano
+            if (value is bool)
+            {
+                //verificando que sea verdero o falso
+                //esto es para poner el texto en "EntStatus"
+                if ((bool) value == true)
+                {
+                    result = "activo";
+                }
+                else
+                {
+                    result = "inactivo";
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AppConvertersXF/AppConvertersXF/AppConvertersXF/ValueConverters/TextToColorConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AppConvertersXF.ValueConverters
+{
+    class TextToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Color clr = Color.Teal;
+            if (value != null)
+            {
+                switch (value.ToString().ToLower())
+                {
+                    case "rojo":
+                        clr = Color.Red;
+                        break;
+                    case "azul":
+                        clr = Color.Blue;
+                        break;
+                    case "verde":
+                        clr = Color.Green;
+                        break;
+                }
+            }
+
+            return clr;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
